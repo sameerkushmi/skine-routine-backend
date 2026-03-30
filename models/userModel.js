5d532b2def08cad2cc02c6e74ae12c62096ba7da
sameer kushmi <csameer795@gmail.com>
2026-03-30 12:23:48 +0545

    pending work mar 30 12:23 PM

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,6 @@ const crypto = require("crypto");
 
 const addressSchema = new mongoose.Schema(
   {
-    fullName: { type: String, required: true },
     phone: { type: String, required: true },
     street: { type: String, required: true },
     city: { type: String, required: true },
