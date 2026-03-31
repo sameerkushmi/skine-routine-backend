cb6d5b4ff6230865463e7f8b12f84b714b86925e
sameer kushmi <csameer795@gmail.com>
2026-03-31 15:17:18 +0545

    pending work mar 31 03:17 PM

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 
 // ================== CORS & Cookie ==================
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: "*",
     credentials: true,
 }));
 app.use(cookieParser());
