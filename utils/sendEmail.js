6754294c6a3a599036a5e99a4a97c3aaf09e65ae
sameer kushmi <csameer795@gmail.com>
2026-04-01 17:01:47 +0545

    pending work mar apr 05:01 PM

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
 const sendEmail = async ({ to, subject, html }) => {
     try {
         await transporter.sendMail({
-            from: `"Skin Routine" <${process.env.SUPPORT_EMAIL}>`,
+            from: `"Skin Routine" <${process.env.EMAIL_USER}>`,
             to,
             subject,
             html,
